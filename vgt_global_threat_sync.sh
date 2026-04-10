82c541e3f1cecf13be36b8f5ef3c1008ea487c98
visiongaiatechnology <31537456+visiongaiatechnology@users.noreply.github.com>
2026-04-10 14:55:30 +0200

    Update vgt_global_threat_sync.sh

--- vgt_global_threat_sync.sh
+++ vgt_global_threat_sync.sh
@@ -3,7 +3,29 @@
 # Modul: GLOBAL_THREAT_SYNC (Daily Threat Intelligence)
 # Status: DIAMANT VGT SUPREME
 # Architektur: Atomarer Swap, Zero-Downtime, O(1) Suchkomplexität
-#
+# LICENSE: AGPLv3 (OPEN SOURCE) - GLOBAL PROLIFERATION PROTOCOL
+# ==============================================================================
+# 
+# Copyright (c) 2026 VISIONGAIATECHNOLOGY
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ==============================================================================
 # Cronjob Setup (Täglich um 04:00 Uhr):
 # 0 4 * * * /root/vgt_global_threat_sync.sh > /dev/null 2>&1
 
